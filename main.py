b5441357ed3eb2a8c04faff7198588c1edd91cb0
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2024-03-29 14:48:43 +0300

    login page

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import sys
 from design import Ui_MainWindow
 
 
-class main_window(QtWidgets.QMainWindow):
+class login(QtWidgets.QMainWindow):
     def __init__(self):
-        super(main_window, self).__init__()
+        super(login, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.btn_clicked)
+        self.ui.log_in_button.clicked.connect(lambda _: self.login(self.ui.login.text(), self.ui.passwd.text()))
 
-    def btn_clicked(self):
-        self.ui.label.setText("Hello world!!")
+    def login(self, login, passwd):
         self.ui.label.adjustSize()
+        print(f"LOGIN: {login}, {passwd}")
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-    win = main_window()
+    win = login()
     win.show()
     sys.exit(app.exec())
