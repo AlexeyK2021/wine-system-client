dfb8a9db90d760fed50292f21cf7e42dfddeb545
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2024-05-06 18:07:25 +0300

    ff page

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ API_IP = "127.0.0.1"
 API_PORT = "5000"
 
 #DEBUG
-LOGIN_AS = ""
+LOGIN_AS = "operator"
--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import sys
+import os
 from PyQt6 import QtWidgets
+from PyQt6.QtCore import QDir
 from PyQt6.QtWidgets import QStackedWidget
 
-from controllers.adminController import AdminPage
 from controllers.loginController import LoginPage
 from controllers.operatorController import OperatorPage
 from controllers.sandboxController import SandBoxPage
 
 if __name__ == '__main__':
+    root = os.path.dirname(os.path.abspath(__file__))
+    QDir.addSearchPath('icons', os.path.join(root, '/designs/icons/'))
+
     app = QtWidgets.QApplication(sys.argv)
     win = QStackedWidget()
 
     login_page = LoginPage()
-    admin_page = AdminPage()
-    # operator_page = OperatorPage()
+    # admin_page = AdminPage()
+    operator_page = OperatorPage()
     sandbox = SandBoxPage()
-    login_page.on_admin_enter = lambda: win.setCurrentIndex(1)
-    # login_page.on_operator_enter = lambda: win.setCurrentIndex(2)
+    # login_page.on_admin_enter = lambda: win.setCurrentIndex(1)
+    login_page.on_operator_enter = lambda: win.setCurrentIndex(1)
     # login_page.on_sandbox = lambda: win.setCurrentIndex(3)
 
     win.addWidget(login_page)
-    win.addWidget(admin_page)
-    # win.addWidget(operator_page)
+    # win.addWidget(admin_page)
+    win.addWidget(operator_page)
     # win.addWidget(sandbox)
 
     win.resize(800, 600)
